9f3974f57cf8ea52faf3c6f27489def606269a12
sangeethashankar03 <20097745@mydbs.ie>
2026-01-30 16:50:24 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 function sumMultiplesInM(f,m){
   let sum=0;
   for(let i=0;i<m.length;i++){
-    for(let j=0;j<f.length;J++){
-      if(m[i]%f[j]===0){
-        sum=sum+m[i];
+    for(let j=0;j<f.length;j++){
+      if(Number(m[i])%Number(f[j])===0){
+        sum=sum+Number(m[i]);
         break;
       }
     }
@@ -14,5 +14,5 @@ function findSum(){
 let f=document.getElementById("fArray").value.split(",");
 let m=document.getElementById("mArray").value.split(",");
 let result= sumMultiplesInM(f,m);
-document.getElementById("output").innerHTML=result;
+document.getElementById("output").innerText=result;
 }
